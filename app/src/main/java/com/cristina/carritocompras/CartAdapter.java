8ff97079f964cc51341ce375e90ab577967e006e
cristinadam1 <cristinadam@correo.ugr.es>
2025-12-03 18:34:31 +0100

    Boton de Eliminar mejorado

--- app/src/main/java/com/cristina/carritocompras/CartAdapter.java
+++ app/src/main/java/com/cristina/carritocompras/CartAdapter.java
@@ -3,7 +3,7 @@ package com.cristina.carritocompras;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
@@ -48,7 +48,7 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder
         ImageView productImage;
         TextView productTitle;
         TextView productPrice;
-        Button removeFromCartButton;
+        ImageButton removeFromCartButton;
 
         public CartViewHolder(@NonNull View itemView) {
             super(itemView);
