d5022d22a0662a8c4c3873303e3c0c644af2cfbd
cristinadam1 <cristinadam@correo.ugr.es>
2025-12-09 19:52:44 +0100

    Documentación en javadoc OrderResponse

--- app/src/main/java/com/cristina/carritocompras/OrderResponse.java
+++ app/src/main/java/com/cristina/carritocompras/OrderResponse.java
@@ -1,8 +1,17 @@
 package com.cristina.carritocompras;
 
+/**
+ * Representa la respuesta del servidor después de crear un nuevo pedido.
+ */
 public class OrderResponse {
+
+    /** El ID del pedido creado en el servidor */
     private int id;
 
+    /**
+     * Obtiene el ID del pedido
+     * @return El ID del pedido
+     */
     public int getId() {
         return id;
     }
