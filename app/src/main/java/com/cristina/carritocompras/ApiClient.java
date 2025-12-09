a8bd4b4b78e34a91b4a484d35318bf566f605414
cristinadam1 <cristinadam@correo.ugr.es>
2025-12-09 19:18:37 +0100

    Documentación en javadoc

--- app/src/main/java/com/cristina/carritocompras/ApiClient.java
+++ app/src/main/java/com/cristina/carritocompras/ApiClient.java
@@ -5,10 +5,29 @@ import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+/**
+ * Cliente centralizado para la configuración y creación de la instancia de Retrofit.
+ * Usa Singleton para asegurar que solo exista una instancia de Retrofit en toda la aplicación.
+ * Centraliza la configuración de la URL base y los tiempos de espera (timeouts) de la conexión.
+ */
 public class ApiClient {
+
+    /** La instancia única de Retrofit. Se inicializa una sola vez */
     private static Retrofit retrofit = null;
+
+    /**
+     * La URL base del servidor. 
+     * Se utiliza la IP 10.0.2.2 para conectar desde el emulador de Android al localhost de mi ordenador
+     */
     private static final String BASE_URL = "http://10.0.2.2:8080/";
 
+    /**
+     * Obtiene la instancia Singleton del cliente Retrofit.
+     * Si la instancia no ha sido creada, la configura con un cliente OkHttpClient
+     * personalizado que incluye timeouts extendidos para evitar errores en conexiones lentas.
+     * 
+     * @return La instancia única de Retrofit, lista para que se use
+     */
     public static Retrofit getClient() {
         if (retrofit == null) {
             OkHttpClient client = new OkHttpClient.Builder()
